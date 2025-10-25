6363faf89c1aff881900c6cc8b85a2e5b9b3b1c1
Kamil <kamilmatysik@outlook.com>
2025-10-25 18:29:19 +0100

    Infinite Cards

--- flashcards.js
+++ flashcards.js
@@ -1,32 +1,83 @@
-flashcard = document.getElementById("flashcard");
+flashcard = document.getElementById("mainFlashcard");
 var cardFlipTime = 150
+addFlashcardClick()
 
 tick = document.getElementById("tick");
 ex = document.getElementById("ex");
 
-flashcard.addEventListener("click", function(){
+function onLoadFunction(){
+    nextFlashcard = document.getElementById("nextFlashcard")
+    nextFlashcard.style.transition = "none"
+    setTimeout(returnTransition,200)
+}
+
+function returnTransition(){
+    nextFlashcard.style.transition = "transform 0.25s linear"
+}
+
+function addFlashcardClick(){
+    flashcard.addEventListener("click", function(){
     if(flashcard.classList.contains("flipped")){
         flashcard.classList.remove("flipped")
-        document.getElementById("flashcardAnswer").style.display = "none"
+        document.getElementById("mainFlashcardAnswer").style.display = "none"
         setTimeout(function(){
-            document.getElementById("flashcardQuestion").style.display = "block"
+            document.getElementById("mainFlashcardQuestion").style.display = "block"
         }, cardFlipTime)
     }
     else{
         flashcard.classList.add("flipped")
-        document.getElementById("flashcardQuestion").style.display = "none" 
+        document.getElementById("mainFlashcardQuestion").style.display = "none" 
         setTimeout(function(){
-            document.getElementById("flashcardAnswer").style.display = "block"
+            document.getElementById("mainFlashcardAnswer").style.display = "block"
         }, cardFlipTime)
     }
-});
+    });
+}
 
 tick.addEventListener("click", function(){
     flashcard.style.transform = "translateX(-200%)";
-    bringNextCard();
+    setTimeout(() => {
+        removeCard();
+        lowerCard();
+    }, 250);
 });
 
 ex.addEventListener("click", function(){
     flashcard.style.transform = "translateX(200%)";
-    bringNextCard();
+    setTimeout(() => {
+        removeCard();
+        lowerCard();
+    }, 250);
 });
+
+function removeCard(){
+    flashcard.remove();
+}
+
+function lowerCard(){
+    document.getElementById("nextFlashcard").id = "mainFlashcard"
+    document.getElementById("nextFlashcardAnswer").id = "mainFlashcardAnswer"
+    document.getElementById("nextFlashcardQuestion").id = "mainFlashcardQuestion"
+    flashcard = document.getElementById("mainFlashcard");
+    flashcard.style.transform = "translateY(0%)"
+
+    flashcard.style.removeProperty("transform");
+    flashcard.style.transition = "transform 0.25s linear";
+
+    addFlashcardClick()
+    createNextCard()
+}
+
+function createNextCard(){
+    newFlashcard = document.createElement("div")
+
+    newFlashcard.id = "nextFlashcard"
+    newFlashcard.classList.add("flashcard")
+    newFlashcard.innerHTML = `
+        <div>
+            <p id="nextFlashcardQuestion" class="flashcardText">Extra q</p>
+            <p id="nextFlashcardAnswer" class="flashcardText">extra a</p>
+        </div>`
+
+    document.body.append(newFlashcard)
+}
